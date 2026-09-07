7e7cd3125d292d82ece949a48f68fc38788d872f
Nouran Alamir <nouranalamir16@gmail.com>
2026-09-08 01:46:25 +0300

    After modifying the project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,27 @@
+
+
+
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using tagr.Models;
+using tagr.Services.Interfaces;
 
 namespace tagr.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return View(products.Take(6).ToList());
         }
 
         public IActionResult Privacy()
@@ -19,7 +32,10 @@ namespace tagr.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
